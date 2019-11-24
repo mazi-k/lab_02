b636e60680401ab03a68d0601da6797c7c908aae
MoraPresence <od34156la@mail.ru>
2019-11-24 15:23:26 +0300

    version 1.1.1

--- sources/source.cpp
+++ sources/source.cpp
@@ -88,7 +88,7 @@ void Cache::set_array(int i) {
 void Cache::set_straight(int size) {
     srand(time(NULL));
     for (int i = 0; i < size; ++i) {
-        _array[i] = rand_r(10);
+        _array[i] = rand_r((unsigned)10);
     }
 }
 
@@ -96,14 +96,14 @@ void Cache::set_straight(int size) {
 void Cache::set_back(int size) {
     srand(time(NULL));
     for (int i = size - 1; i >= 0; --i) {
-        _array[i] = rand_r(10);
+        _array[i] = rand_r((unsigned)10);
     }
 }
 
 void Cache::set_random(int size, std::vector <size_t> &myrand) {
     srand(time(NULL));
     for (int i = 0; i < size; ++i) {
-        _array[myrand[i]] = rand_r(10);
+        _array[myrand[i]] = rand_r((unsigned)10);
     }
 }
 
